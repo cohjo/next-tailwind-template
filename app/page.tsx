d92a8a7bdcd28e135844978c83cc36810e0c276e
Conor Johnston <chjohnston96@gmail.com>
2022-12-07 21:04:17 -0500

    testing links work

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,17 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>hello</main>
+    <div>
+      <nav>
+        <Link href="/about">about</Link>
+        <Link href="/contact">contact</Link>
+      </nav>
+      <main>hello</main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <footer></footer>
     </div>
   )
 }
