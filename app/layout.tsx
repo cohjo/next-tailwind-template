771e5d6a0983e91a9072c181615a76763042c8f2
Conor Johnston <chjohnston96@gmail.com>
2022-12-14 21:48:06 -0500

    add inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 // import '../output.css' // needed when using turbopack
 import Navbar from './components/client/Navbar'
+import { Inter } from '@next/font/google'
 import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
@@ -8,13 +11,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html className="box-border h-full p-2 sm:p-4" lang="en">
+    <html className={'box-border h-full p-2 sm:p-4'} lang="en">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className="w-full h-fit">
+      <body className={inter.className}>
         <Navbar />
         <div className="flex flex-col items-center justify-start w-full h-full">
           {children}
